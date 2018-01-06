5f2d56e4c2b480df2bbcd859358b5a3eba73610b
yuqing <stltqhs@163.com>
2018-01-06 18:27:49 +0800

    Mapper对象

---
+++ magic-mybatis/src/main/java/com/yuqing/magic/mybatis/mapper/common/DirtyUpdateMapper.java
@@ -0,0 +1,12 @@
+package com.yuqing.magic.mybatis.mapper.common;
+
+/**
+ * 脏值更新的mapper
+ * @author yuqing
+ *
+ * @since 1.0.1
+ */
+public interface DirtyUpdateMapper {
+
+    void updateByDirtySelective(Object object);
+}
---
+++ magic-mybatis/src/main/java/com/yuqing/magic/mybatis/mapper/common/VersionUpdateMapper.java
@@ -0,0 +1,10 @@
+package com.yuqing.magic.mybatis.mapper.common;
+
+/**
+ * 具备版本号更新的mapper，以实现乐观锁
+ * @author yuqing
+ *
+ * @since 1.0.1
+ */
+public interface VersionUpdateMapper {
+}
