10f984335fd139a96ab9d261b13ebbaf17325cc8
yuqing <stltqhs@163.com>
2018-01-14 17:58:52 +0800

    日志配置

---
+++ magic-log-config/src/test/java/com/yuqing/magic/log/config/AppTest.java
@@ -0,0 +1,24 @@
+package com.yuqing.magic.log.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author YutsingLee
+ * @date 2018-01-14
+ */
+@RunWith(JUnit4.class)
+public class AppTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(AppTest.class);
+
+    @Test
+    public void test() {
+        logger.debug("Debug Test Log");
+        logger.info("Info Test Log");
+    }
+
+}
